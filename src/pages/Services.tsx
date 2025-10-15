c04d6f6703fd7ed02dbc49c2e9bd86dea51c3914
PetsPlanet <108582217+efeluxy@users.noreply.github.com>
2025-10-15 14:22:49 +0200

    Título servicios modificado
    Se modifica el tamaño del título de la página Servicios

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -3,9 +3,12 @@ import ServicesCard from "../components/ServicesCard";
 
 export default function Services() {
   return (
-    <section id='Servicios'>
+    <section 
+      id="Servicios"
+      className="flex justify-center min-h-screen items-center pt-32 sm:pt-40"
+    >
       <div className="w-full px-6 py-6 md:px-40 md:py-15">
-        <h2 className="text-4xl md:text-6xl font-extrabold text-center mb-16">
+        <h2 className="text-2xl md:text-4xl font-extrabold text-center mb-16">
           Soluciones digitales hechas a tu medida
         </h2>
         <div className="flex flex-col md:flex-row gap-8 md:gap-10 items-center">
